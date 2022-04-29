417eaddc00556e21ef0ed8bd618b98875264f482
kyoapps-kyo <kyo@kyoapps.com>
2022-04-29 17:55:02 +0900

    text

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,12 +1,15 @@
 <template>
-    <h3>Kyo Apps</h3>
+    <h3>{{ name }}</h3>
+    <input type="text" v-model="content">
+    <span>-> Content {{ content }}</span>
 </template>
 
 <script>
 export default {
     data() {
         return {
-
+            name: 'Kyo Apps',
+            content: '起飞了',
         }
     }     
 
